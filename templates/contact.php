d9ff8cb1dbbbac5dba3b5466ecf719a08fa8e66f
Felipe Arosemena <felipe@briteweb.com>
2016-06-12 22:44:18 -0700

    missing quots

--- templates/contact.php
+++ templates/contact.php
@@ -7,6 +7,6 @@
 $context = Timber::get_context();
 $post    = new TimberPost();
 $context['post'] = $post;
-$context['gf_contact_form'] = class_exists(GFAPI) ? GFAPI::get_form(1) : [];
+$context['gf_contact_form'] = class_exists('GFAPI') ? GFAPI::get_form(1) : [];
 
 Timber::render('contact.twig', $context);
