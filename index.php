17753e1b47f5bfaccd8dae98723326e05373cbf0
Felipe Arosemena <felipe@briteweb.com>
2016-07-05 21:48:58 -0700

    setting up blog

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 
 $context                    = Timber::get_context();
+$context['post']            = Timber::get_post(get_option('page_for_posts'));
 $context['posts']           = Timber::get_posts();
-$context['page_title']      = get_the_title( get_option('page_for_posts', true) );
 $context['sidebar']         = Timber::get_widgets('sidebar-blog'); // slug of sidebar to be included in this template
 $context['categories_list'] = wp_list_categories('echo=0&title_li=');  
 
