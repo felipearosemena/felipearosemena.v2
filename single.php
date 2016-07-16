9ebf35e8c18df735393cc8cf773305f794aced6d
Felipe Arosemena <hello@felipearosemena.com>
2016-07-16 11:26:56 -0700

    Configure Next Post Variable

--- single.php
+++ single.php
@@ -4,15 +4,12 @@ $context = Timber::get_context();
 $post    = new TimberPost();
 $context['post'] = $post;   
 
-// Previous/Next post links
-$prev_post =  get_adjacent_post(false,'',false);
-$next_post =  get_adjacent_post(false,'',true);
 
-if(is_object($prev_post)) {
-    $context['prev_post'] = new TimberPost($prev_post->ID);
-} 
-if(is_object($next_post) ) {
-    $context['next_post'] = new TimberPost($next_post->ID);
+$next_post =  get_adjacent_post(false,'',true);
+if(!is_object($next_post) ) {
+  $context['next_post'] = Timber::get_posts('post_type=post&posts_per_page=1&order=ASC')[0];
+} else {
+  $context['next_post'] = new TimberPost($next_post->ID);
 }
 
 Timber::render('page.twig', $context);
