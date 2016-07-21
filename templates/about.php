4055a45b5a0904311a4fd39c4405b4637746af4b
Felipe Arosemena <hello@felipearosemena.com>
2016-07-20 21:09:08 -0700

    last tweaks before initial releas of new tiles layout

--- templates/about.php
+++ templates/about.php
@@ -8,6 +8,8 @@ $context = Timber::get_context();
 $post    = new TimberPost();
 $context['post'] = $post;
 
+$context['related_project'] = Timber::get_post('post_type=project&posts_per_page=1');
+
 Timber::render('about.twig', $context);
 
 
